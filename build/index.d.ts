7a265c72ef6f453e92fa5c7cf4fd980a2361dca4
Danny Davidson <chief@onceknown.com>
2022-03-15 00:48:29 -0500

    no interface, just namespace

--- build/index.d.ts
+++ build/index.d.ts
@@ -5,79 +5,77 @@ import { Block } from "./lib/Node";
 
 declare namespace strip {
 
-    export default interface strip {
-        /**
-         * Strip all code comments from the given `input`, including protected
-         * comments that start with `!`, unless disabled by setting `options.keepProtected`
-         * to true.
-         *
-         * ```js
-         * const str = strip('const foo = "bar";// this is a comment\n /* me too *\/');
-         * console.log(str);
-         * // => 'const foo = "bar";'
-         * ```
-         * @param string - string from which to strip comments
-         * @param [options] - passed to [extract-comments][extract-comments]
-         */
-        (input: string, options?: strip.Options): string;
+    /**
+     * Strip all code comments from the given `input`, including protected
+     * comments that start with `!`, unless disabled by setting `options.keepProtected`
+     * to true.
+     *
+     * ```js
+     * const str = strip('const foo = "bar";// this is a comment\n /* me too *\/');
+     * console.log(str);
+     * // => 'const foo = "bar";'
+     * ```
+     * @param string - string from which to strip comments
+     * @param [options] - passed to [extract-comments][extract-comments]
+     */
+    export default function strip(input: string, options?: strip.Options): string;
 
-        /**
-         * Strip only block comments.
-         *
-         * ```js
-         * const str = strip.block('const foo = "bar";// this is a comment\n /* me too *\/');
-         * console.log(str);
-         * // => 'const foo = "bar";// this is a comment'
-         * ```
-         * @param `input` string from which to strip comments
-         * @param  [`options`] pass `opts.keepProtected: true` to keep ignored comments (e.g. `/*!`)
-         */
-        block(input: string, options?: Options): string;
+    /**
+     * Strip only block comments.
+     *
+     * ```js
+     * const str = strip.block('const foo = "bar";// this is a comment\n /* me too *\/');
+     * console.log(str);
+     * // => 'const foo = "bar";// this is a comment'
+     * ```
+     * @param `input` string from which to strip comments
+     * @param  [`options`] pass `opts.keepProtected: true` to keep ignored comments (e.g. `/*!`)
+     */
+    export function block(input: string, options?: Options): string;
 
-        /**
-         * Strip only line comments.
-         *
-         * ```js
-         * const str = strip.line('const foo = "bar";// this is a comment\n /* me too *\/');
-         * console.log(str);
-         * // => 'const foo = "bar";\n/* me too *\/'
-         * ```
-         * @param  `input` string from which to strip comments
-         * @param  [`options`] pass `opts.keepProtected: true` to keep ignored comments (e.g. `//!`)
-         */
-        line(input: string, options?: Options): string;
+    /**
+     * Strip only line comments.
+     *
+     * ```js
+     * const str = strip.line('const foo = "bar";// this is a comment\n /* me too *\/');
+     * console.log(str);
+     * // => 'const foo = "bar";\n/* me too *\/'
+     * ```
+     * @param  `input` string from which to strip comments
+     * @param  [`options`] pass `opts.keepProtected: true` to keep ignored comments (e.g. `//!`)
+     */
+    export function line(input: string, options?: Options): string;
 
-        /**
-         * Strip the first comment from the given `input`. Or, if `opts.keepProtected` is true,
-         * the first non-protected comment will be stripped.
-         *
-         * ```js
-         * const output = strip.first(input, { keepProtected: true });
-         * console.log(output);
-         * // => '//! first comment\nfoo; '
-         * ```
-         * @param  `input` string from which to strip comments
-         * @param [`options`] pass `opts.keepProtected: true` to keep comments with `!`
-         */
-        first(input: string, options?: Options): string;
+    /**
+     * Strip the first comment from the given `input`. Or, if `opts.keepProtected` is true,
+     * the first non-protected comment will be stripped.
+     *
+     * ```js
+     * const output = strip.first(input, { keepProtected: true });
+     * console.log(output);
+     * // => '//! first comment\nfoo; '
+     * ```
+     * @param  `input` string from which to strip comments
+     * @param [`options`] pass `opts.keepProtected: true` to keep comments with `!`
+     */
+    export function first(input: string, options?: Options): string;
 
-        /**
-         * Parses a string and returns a basic CST (Concrete Syntax Tree).
-         *
-         * ```js
-         * const strip = require('..');
-         * const str = strip.block('const foo = "bar";// this is a comment\n /* me too *\/');
-         * console.log(str);
-         * // => 'const foo = "bar";// this is a comment'
-         * ```
-         * @name  .block
-         * @param  {String} `input` string from which to strip comments
-         * @param  {Object} `options` pass `opts.keepProtected: true` to keep ignored comments (e.g. `/*!`)
-         * @return {String} modified string
-         * @api public
-         */
-        parse(input: string, options?: Options): Block;
-    }
+    /**
+     * Parses a string and returns a basic CST (Concrete Syntax Tree).
+     *
+     * ```js
+     * const strip = require('..');
+     * const str = strip.block('const foo = "bar";// this is a comment\n /* me too *\/');
+     * console.log(str);
+     * // => 'const foo = "bar";// this is a comment'
+     * ```
+     * @name  .block
+     * @param  {String} `input` string from which to strip comments
+     * @param  {Object} `options` pass `opts.keepProtected: true` to keep ignored comments (e.g. `/*!`)
+     * @return {String} modified string
+     * @api public
+     */
+    export function parse(input: string, options?: Options): Block;
 
     export interface Options {
         /**
